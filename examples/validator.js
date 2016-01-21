2c1aba5fe9fc7671586c7083e0128aca9593dfe2
Harminder Virk <virk.officials@gmail.com>
2016-01-21 13:35:55 +0530

    docs(examples): added examples

---
+++ examples/validator.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const indicative = require('../')
+
+const data = {
+  profile: {
+    email: 'bar.sneak+foo@googlemail.com'
+  }
+}
+
+const rules = {
+  'profile.email': 'email',
+  username: 'required'
+}
+
+indicative
+  .validate(data, rules)
+  .catch(function (errors) {
+    console.log('\n')
+    console.log('single validation >>>')
+    console.log(errors)
+    console.log('\n\n')
+  })
+
+indicative
+  .validateAll(data, rules)
+  .catch(function (errors) {
+    console.log('all validations >>>')
+    console.log(errors)
+  })
+
+console.log(indicative.is.email(data.profile.email))
