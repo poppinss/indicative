a3767ea89b23761ff4cd5bab0290d8827421fdd6
Harminder Virk <virk.officials@gmail.com>
2016-01-21 13:25:03 +0530

    style(exports): now exporting selected methods from index.js file

--- index.js
+++ index.js
@@ -6,4 +6,16 @@
  * Copyright(c) 2015-2015 Harminder Virk
  * MIT Licensed
 */
-module.exports = require('./src/Validator')
+const Validator = require('./src/Validator')
+const SanitizationFilters = require('./src/Sanitization/filters')
+const Sanitization = require('./src/Sanitization')
+
+module.exports = {
+  validate: Validator.validate,
+  validateAll: Validator.validateAll,
+  extend: Validator.extend,
+  is: Validator.is,
+  'is.extend': Validator.is.extend,
+  sanitize: Sanitization.sanitize,
+  sanitizor: SanitizationFilters
+}
