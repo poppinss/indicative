002472be9fa282cf54f4811c6ef66bad2d076ce9
Andrés M. Gómez <andres86mauricio@hotmail.com>
2017-06-18 20:38:37 -0500

    Removed unnecessary escape in regular expressions

--- src/Parser/index.js
+++ src/Parser/index.js
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
 */
 
-const arrayExpressionRegex = /(\w[^\.\*]+)(\.\*\.?)(.+)?/
+const arrayExpressionRegex = /(\w[^.*]+)(\.\*\.?)(.+)?/
 const _ = require('lodash')
 
 let Parser = exports = module.exports = {}
