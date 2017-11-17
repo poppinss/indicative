f9e2705fc7e17fa9bceb065237bc0d932d639892
Harminder Virk <virk.officials@gmail.com>
2017-11-17 21:27:41 +0530

    chore(benchmarks): add benchmarks for refactoring

---
+++ benchmarks/validate.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Benchmark = require('benchmark')
+const suite = new (Benchmark.Suite)()
+
+const { validate } = require('../index')
+const rules = {
+  username: 'required|alpha',
+  email: 'required|email',
+  password: 'required|min:6|max:10'
+}
+const data = {}
+
+suite.add('validate', function () {
+  validate(rules, data)
+})
+.on('cycle', function (event) {
+  console.log(String(event.target))
+})
+.run({ 'async': true })
