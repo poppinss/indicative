c11f1736ae908baa23e984adb0ca7ea4fb09682e
Onyeka Ijeh <onyekaijeh@yahoo.com>
2018-06-10 14:01:17 +0100

    convert input to number before validating isnumber

--- src/raw/isNumber.js
+++ src/raw/isNumber.js
@@ -1,1 +1,7 @@
-export default (input) => typeof (input) === 'number' && !isNaN(input)
+export default (input, strict) => {
+  const isNumber = typeof input === 'number'
+  if (!isNumber && !strict) {
+    return !isNaN(input)
+  }
+  return isNumber
+}
