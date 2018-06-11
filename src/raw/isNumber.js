1f6cb14841f89f4bf010d2159bbe26be5e913777
Onyeka Ijeh <onyekaijeh@yahoo.com>
2018-06-11 15:31:41 +0100

    refactor and add tests

--- src/raw/isNumber.js
+++ src/raw/isNumber.js
@@ -1,5 +1,6 @@
 export default (input, strict) => {
-  const isNumber = typeof input === 'number'
+  const isNumber = typeof input === 'number' && !isNaN(input)
+  if (input === true || input === false) return false
   if (!isNumber && !strict) {
     return !isNaN(input)
   }
