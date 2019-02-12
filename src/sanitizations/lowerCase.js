7329a2e0d8f94ccafe571f598430495e2a1e031e
Andrew Jo <andrewjo@gmail.com>
2019-02-12 02:13:15 -0800

    feat(sanitizations): add lowerCase and upperCase method (#211)

---
+++ src/sanitizations/lowerCase.js
@@ -0,0 +1,40 @@
+/**
+ * Converts a value to lower case. This sanitization rule is locale aware.
+ * If value is not a string, then it will return as is.
+ *
+ * [source, js]
+ * ----
+ * const sanitizationRules = {
+ *   hexCode: 'lowerCase'
+ * }
+ *
+ * // or
+ * const sanitizationRules = {
+ *   hexCode: [
+ *     rule('lowerCase')
+ *   ]
+ * }
+ * ----
+ *
+ * You may also specify a locale.
+ *
+ * [source, js]
+ * ----
+ * const sanitizationRules = {
+ *   label: 'lowerCase:fr-CA'
+ * }
+ *
+ * // or
+ * const sanitizationRules = {
+ *   label: [
+ *     rule('lowerCase', 'fr-CA')
+ *   ]
+ * }
+ */
+export default (value, args) => {
+  if (typeof value !== 'string') {
+    return value
+  }
+
+  return value.toLocaleLowerCase(args)
+}
