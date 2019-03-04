b2a826e6ece3a693dbf7e5e4f4dbd35d2a5a9ce8
thetutlage <virk.officials@gmail.com>
2019-03-04 15:40:33 +0530

    feat(parser): add messages parser
    BREAKING CHANGE: messages tree is similar to schema tree and hence
    we need to rename `rules` property to `rhs` for a generic name

--- packages/parser/test/parser.spec.ts
+++ packages/parser/test/parser.spec.ts
@@ -10,10 +10,10 @@
 */
 
 import * as test from 'japa'
-import { parser as parse } from '../src/main'
+import { schemaParser as parse, messagesParser } from '../src/main'
 import { rule } from '../src/main'
 
-test.group('Parser', () => {
+test.group('Parser | schema', () => {
   test('parse simple field rules', (assert) => {
     const output = parse({
       username: 'required',
@@ -22,7 +22,7 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       username: {
         type: 'literal',
-        rules: [{
+        rhs: [{
           name: 'required',
           args: [],
         }],
@@ -38,7 +38,7 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       username: {
         type: 'literal',
-        rules: [{
+        rhs: [{
           name: 'required',
           args: [],
         }],
@@ -54,7 +54,7 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       username: {
         type: 'literal',
-        rules: [{
+        rhs: [{
           name: 'min',
           args: [10],
         }],
@@ -70,7 +70,7 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       username: {
         type: 'literal',
-        rules: [
+        rhs: [
           {
             name: 'required',
             args: [],
@@ -95,7 +95,7 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       username: {
         type: 'literal',
-        rules: [
+        rhs: [
           {
             name: 'required',
             args: [],
@@ -117,10 +117,10 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       user: {
         type: 'object',
-        rules: [],
+        rhs: [],
         children: {
           profile: {
-            rules: [
+            rhs: [
               {
                 name: 'required',
                 args: [],
@@ -139,14 +139,14 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       'users': {
         type: 'array',
-        rules: [],
+        rhs: [],
         each: {
           '*': {
-            rules: [],
+            rhs: [],
             children: {
               username: {
                 type: 'literal',
-                rules: [
+                rhs: [
                   {
                     name: 'required',
                     args: [],
@@ -166,18 +166,18 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       users: {
         type: 'array',
-        rules: [],
+        rhs: [],
         each: {
           '*': {
-            rules: [],
+            rhs: [],
             children: {
               profile: {
                 type: 'object',
-                rules: [],
+                rhs: [],
                 children: {
                   username: {
                     type: 'literal',
-                    rules: [{
+                    rhs: [{
                       name: 'required',
                       args: [],
                     }],
@@ -197,10 +197,10 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       users: {
         type: 'array',
-        rules: [],
+        rhs: [],
         each: {
           '*': {
-            rules: [{
+            rhs: [{
               name: 'required',
               args: [],
             }],
@@ -220,13 +220,13 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       users: {
         type: 'array',
-        rules: [{
+        rhs: [{
           name: 'array',
           args: [],
         }],
         each: {
           '*': {
-            rules: [{
+            rhs: [{
               name: 'required',
               args: [],
             }],
@@ -248,27 +248,27 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       users: {
         type: 'array',
-        rules: [{
+        rhs: [{
           name: 'array',
           args: [],
         }],
         each: {
           '*': {
-            rules: [{
+            rhs: [{
               name: 'required',
               args: [],
             }],
             children: {
               profile: {
                 type: 'object',
-                rules: [{
+                rhs: [{
                   name: 'object',
                   args: [],
                 }],
                 children: {
                   age: {
                     type: 'literal',
-                    rules: [
+                    rhs: [
                       {
                         name: 'required',
                         args: [],
@@ -297,11 +297,11 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       user: {
         type: 'object',
-        rules: [],
+        rhs: [],
         children: {
           profile: {
             type: 'object',
-            rules: [
+            rhs: [
               {
                 name: 'required',
                 args: [],
@@ -310,7 +310,7 @@ test.group('Parser', () => {
             children: {
               type: {
                 type: 'literal',
-                rules: [
+                rhs: [
                   {
                     name: 'required',
                     args: [],
@@ -333,11 +333,11 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       user: {
         type: 'object',
-        rules: [],
+        rhs: [],
         children: {
           profile: {
             type: 'object',
-            rules: [
+            rhs: [
               {
                 name: 'required',
                 args: [],
@@ -346,7 +346,7 @@ test.group('Parser', () => {
             children: {
               type: {
                 type: 'literal',
-                rules: [
+                rhs: [
                   {
                     name: 'required',
                     args: [],
@@ -366,14 +366,14 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       users: {
         type: 'array',
-        rules: [],
+        rhs: [],
         each: {
           '0': {
-            rules: [],
+            rhs: [],
             children: {
               username: {
                 type: 'literal',
-                rules: [{
+                rhs: [{
                   name: 'required',
                   args: [],
                 }],
@@ -394,17 +394,17 @@ test.group('Parser', () => {
     assert.deepEqual(output, {
       users: {
         type: 'array',
-        rules: [],
+        rhs: [],
         each: {
           '0': {
-            rules: [{
+            rhs: [{
               name: 'required',
               args: [],
             }],
             children: {
               username: {
                 type: 'literal',
-                rules: [{
+                rhs: [{
                   name: 'required',
                   args: [],
                 }],
@@ -432,5 +432,183 @@ test.group('Parser', () => {
     })
 
     assert.throw(output, 'cannot reshape user array to an object')
+  })
+})
+
+test.group('Parser | messages', () => {
+  test('parse messages for fields', (assert) => {
+    const output = messagesParser({
+      'username.required': 'Username is required',
+    })
+
+    assert.deepEqual(output, {
+      rules: {},
+      named: {
+        username: {
+          type: 'literal',
+          rhs: {
+            required: 'Username is required',
+          },
+        },
+      },
+    })
+  })
+
+  test('parse messages for nested fields', (assert) => {
+    const output = messagesParser({
+      'username.profile.required': 'Profile is required',
+    })
+
+    assert.deepEqual(output, {
+      rules: {},
+      named: {
+        username: {
+          type: 'object',
+          rhs: {},
+          children: {
+            profile: {
+              type: 'literal',
+              rhs: {
+                required: 'Profile is required',
+              },
+            },
+          },
+        },
+      },
+    })
+  })
+
+  test('parse messages for nested fields and top level fields', (assert) => {
+    const output = messagesParser({
+      'username.profile.required': 'Profile is required',
+      'username.required': 'Username is required',
+    })
+
+    assert.deepEqual(output, {
+      rules: {},
+      named: {
+        username: {
+          type: 'object',
+          rhs: {
+            required: 'Username is required',
+          },
+          children: {
+            profile: {
+              type: 'literal',
+              rhs: {
+                required: 'Profile is required',
+              },
+            },
+          },
+        },
+      },
+    })
+  })
+
+  test('parse messages for array', (assert) => {
+    const output = messagesParser({
+      'users.*.username.required': 'Username is required',
+    })
+
+    assert.deepEqual(output, {
+      rules: {},
+      named: {
+        users: {
+          type: 'array',
+          rhs: {},
+          each: {
+            '*': {
+              rhs: {},
+              children: {
+                username: {
+                  type: 'literal',
+                  rhs: {
+                    required: 'Username is required',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+  })
+
+  test('parse messages for indexed arrays', (assert) => {
+    const output = messagesParser({
+      'users.0.username.required': 'Main username is required',
+      'users.*.username.required': 'Username is required',
+    })
+
+    assert.deepEqual(output, {
+      rules: {},
+      named: {
+        users: {
+          type: 'array',
+          rhs: {},
+          each: {
+            '0': {
+              rhs: {},
+              children: {
+                username: {
+                  type: 'literal',
+                  rhs: {
+                    required: 'Main username is required',
+                  },
+                },
+              },
+            },
+            '*': {
+              rhs: {},
+              children: {
+                username: {
+                  type: 'literal',
+                  rhs: {
+                    required: 'Username is required',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+  })
+
+  test('parse messages array literals', (assert) => {
+    const output = messagesParser({
+      'users.*.min': 'Array must have one item',
+    })
+
+    assert.deepEqual(output, {
+      rules: {},
+      named: {
+        users: {
+          type: 'array',
+          rhs: {},
+          each: {
+            '*': {
+              rhs: {
+                min: 'Array must have one item',
+              },
+              children: {},
+            },
+          },
+        },
+      },
+    })
+  })
+
+  test('parse messages for just rules', (assert) => {
+    const output = messagesParser({
+      'required': 'The field is required',
+    })
+
+    assert.deepEqual(output, {
+      rules: {
+        required: 'The field is required',
+      },
+      named: {},
+    })
   })
 })
