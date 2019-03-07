f9871905c9d6d6613e37fae420085dd2a9c0c2f0
thetutlage <virk.officials@gmail.com>
2019-03-07 12:13:44 +0530

    chore: use lerna monorepo

--- static/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwindcss = require('tailwindcss')
-const path = require('path')
-
-module.exports = {
-  plugins: [
-    require('postcss-easy-import'),
-    tailwindcss(path.join(__dirname, './tailwind.js'))
-  ]
-}
