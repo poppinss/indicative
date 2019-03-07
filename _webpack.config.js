3566ed900862d5af78656311715c62565fbe6f72
thetutlage <virk.officials@gmail.com>
2019-03-07 12:13:44 +0530

    chore: use lerna monorepo

---
+++ _webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = function config (userConfig) {
+  return require('extend')(true, {}, {
+    entry: './index.ts',
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    mode: 'production',
+    output: {
+      filename: 'index.js',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    }
+  }, userConfig)
+}
