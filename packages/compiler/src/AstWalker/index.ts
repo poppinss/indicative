22893b874a70670f0d63031008a9fbfc49939da6
Harminder virk <virk.officials@gmail.com>
2019-03-26 17:37:10 +0530

    refactor(compile): complete refactoring

---
+++ packages/compiler/src/AstWalker/index.ts
@@ -0,0 +1,177 @@
+/*
+* indicative
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import { ParsedSchema, SchemaNodeLiteral, SchemaNodeObject, SchemaNodeArray } from 'indicative-parser'
+import { NodeConsumer, ArrayChildWrapper } from '../Contracts/AstWalker'
+
+/**
+ * Process the literal node inside the tree.
+ */
+function literalNodeConsumer<Output extends any, NodeArgs extends any[]> (
+  node: SchemaNodeLiteral,
+  field: string,
+  nodeArgs: NodeArgs,
+  nodeConsumer: NodeConsumer<NodeArgs, Output>,
+  dotPath: string[],
+  resetPaths: string[],
+): Output[] {
+  return node.rules.map((rule) => nodeConsumer(rule, field, 'literal', nodeArgs, dotPath, resetPaths))
+}
+
+/**
+ * Process the object node inside the tree.
+ */
+function objectNodeConsumer<Output extends any, NodeArgs extends any[]> (
+  node: SchemaNodeObject,
+  field: string,
+  nodeArgs: NodeArgs,
+  nodeConsumer: NodeConsumer<NodeArgs, Output>,
+  arrayChildWrapper: any,
+  dotPath: string[],
+  resetPaths: string[],
+): Output[] {
+  let executors: Output[] = []
+
+  /**
+   * Consume all rules on the object node itself
+   */
+  if (node.rules.length) {
+    executors = node.rules.map((rule) => {
+      return nodeConsumer(rule, field, 'object', nodeArgs, dotPath, resetPaths)
+    })
+  }
+
+  dotPath = dotPath.concat(field)
+
+  /**
+   * Recursively process child
+   */
+  const childExecutors = astWalker<Output, NodeArgs>(
+    node.children,
+    nodeArgs,
+    nodeConsumer,
+    arrayChildWrapper,
+    dotPath,
+    resetPaths,
+  )
+
+  executors = executors.concat(childExecutors)
+  return executors
+}
+
+/**
+ * Process the array node inside the tree.
+ */
+function arrayNodeConsumer<Output extends any, NodeArgs extends any[]> (
+  node: SchemaNodeArray,
+  field: string,
+  nodeArgs: NodeArgs,
+  nodeConsumer: NodeConsumer<NodeArgs, Output>,
+  arrayChildWrapper: any,
+  dotPath: string[],
+  resetPaths: string[],
+): Output[] {
+  let executors: Output[] = []
+
+  /**
+   * Consume all rules on the array node itself
+   */
+  if (node.rules.length) {
+    executors = node.rules.map((rule) => nodeConsumer(rule, field, 'array', nodeArgs, dotPath, resetPaths))
+  }
+
+  dotPath = dotPath.concat(field)
+
+  const wrappedExecutors = Object.keys(node.each)
+    .reduce((result: Output[], index: string) => {
+      let rulesExecutor: Output[] = []
+
+      /**
+       * Handling literal values inside the array
+       */
+      if (node.each[index].rules.length) {
+        rulesExecutor = astWalker(
+          { ['*']: { type: 'literal', rules: node.each[index].rules } },
+          nodeArgs,
+          nodeConsumer,
+          arrayChildWrapper,
+          [],
+          resetPaths.concat(dotPath).concat(index),
+        )
+      }
+
+      /**
+       * Handling objects inside the array
+       */
+      const eachExecutors = astWalker(
+        node.each[index].children,
+        nodeArgs,
+        nodeConsumer,
+        arrayChildWrapper,
+        [],
+        resetPaths.concat(dotPath).concat(index),
+      )
+
+      result.push(arrayChildWrapper(rulesExecutor.concat(eachExecutors), dotPath, index))
+      return result
+    }, [])
+
+  executors = executors.concat(wrappedExecutors)
+  return executors
+}
+
+/**
+ * AST walkers recursively loop throughs all the nodes inside the `parsedSchema`
+ * tree and executes the `nodeConsumer` defined by the consumer of this
+ * module.
+ *
+ * The `nodeConsumer` can decide how to consume the nodes and the collection of
+ * return value from the consumer is returned as an array.
+ *
+ * 1. `nodeConsumer`: A function that consumes a single node from the parsed tree.
+ * 2. `nodeArgs`: Custom array of arguments to be passed to `nodeConsumer`.
+ * 3. `arrayChildWrapper`: Array child wrapper is used to wrap the child nodes of a array.
+ *
+ * The last arguments of this function are optional and used internally with recursion in
+ * play.
+ */
+export function astWalker <Output extends any, NodeArgs extends any[]> (
+  tree: ParsedSchema,
+  nodeArgs: NodeArgs,
+  nodeConsumer: NodeConsumer<NodeArgs, Output>,
+  arrayChildWrapper: ArrayChildWrapper<Output>,
+  nodePath: string[] = [],
+  resetPaths: string[] = [],
+): Output[] {
+  return Object
+    .keys(tree)
+    .reduce((result: Output[], field: string) => {
+      const node = tree[field]
+
+      if (node.type === 'literal') {
+        result = result.concat(
+          literalNodeConsumer(node, field, nodeArgs, nodeConsumer, nodePath, resetPaths),
+        )
+      }
+
+      if (node.type === 'object') {
+        result = result.concat(
+          objectNodeConsumer(node, field, nodeArgs, nodeConsumer, arrayChildWrapper, nodePath, resetPaths),
+        )
+      }
+
+      if (node.type === 'array') {
+        result = result.concat(
+          arrayNodeConsumer(node, field, nodeArgs, nodeConsumer, arrayChildWrapper, nodePath, resetPaths),
+        )
+      }
+
+      return result
+    }, [])
+}
---
+++ packages/compiler/test/ast-walker.spec.ts
@@ -0,0 +1,378 @@
+/*
+* indicative-compiler
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import * as test from 'japa'
+import { astWalker } from '../src/AstWalker'
+import { NodeConsumer, ArrayChildWrapper } from '../src/Contracts/AstWalker'
+import { rulesParser } from 'indicative-parser'
+
+const consumer: NodeConsumer<any[], any> = (
+  rule,
+  field,
+  type,
+  args,
+  dotPath,
+  resetPaths,
+) => {
+  let messageField = field
+  const initialPaths = resetPaths.concat(dotPath)
+
+  if (initialPaths.length) {
+    messageField = field === '*' ? initialPaths.join('.') : `${initialPaths.join('.')}.${field}`
+  }
+
+  return { rule, field, dotPath, type, message: args[0][messageField][rule.name] }
+}
+
+const arrayWrapper: ArrayChildWrapper<any> = (
+  child,
+  dotPath,
+  index,
+) => {
+  return child.map((node) => {
+    return {
+      child: node,
+      dotPath,
+      index,
+    }
+  })
+}
+
+test.group('Ast walker', () => {
+  test('return array of top level functions for each literal leaf', (assert) => {
+    const parsedRules = rulesParser({
+      username: 'required',
+      email: 'required',
+    })
+
+    const args = [
+      {
+        username: { required: 'username is required' },
+        email: { required: 'email is required' },
+      },
+    ]
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'username',
+        dotPath: [],
+        type: 'literal',
+        message: 'username is required',
+      },
+      {
+        rule: { name: 'required', args: [] },
+        field: 'email',
+        dotPath: [],
+        type: 'literal',
+        message: 'email is required',
+      },
+    ])
+  })
+
+  test('return array of top level functions for object leaf', (assert) => {
+    const parsedRules = rulesParser({
+      'profile': 'required',
+      'profile.username': 'required',
+    })
+
+    const args = [
+      {
+        profile: { required: 'profile is required' },
+        'profile.username': { required: 'profile username is required' },
+      },
+    ]
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'profile',
+        dotPath: [],
+        type: 'object',
+        message: 'profile is required',
+      },
+      {
+        rule: { name: 'required', args: [] },
+        field: 'username',
+        dotPath: ['profile'],
+        type: 'literal',
+        message: 'profile username is required',
+      },
+    ])
+  })
+
+  test('return array of top level functions for nested object leaf', (assert) => {
+    const parsedRules = rulesParser({
+      'profile': 'required',
+      'profile.social': 'required',
+      'profile.social.type': 'enum:facebook,twitter',
+    })
+
+    const args = [
+      {
+        profile: { required: 'profile is required' },
+        'profile.social': { required: 'social profile is required' },
+        'profile.social.type': { enum: 'one of facebook or twitter' },
+      },
+    ]
+
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'profile',
+        dotPath: [],
+        type: 'object',
+        message: 'profile is required',
+      },
+      {
+        rule: { name: 'required', args: [] },
+        field: 'social',
+        dotPath: ['profile'],
+        type: 'object',
+        message: 'social profile is required',
+      },
+      {
+        rule: { name: 'enum', args: ['facebook', 'twitter'] },
+        field: 'type',
+        dotPath: ['profile', 'social'],
+        type: 'literal',
+        message: 'one of facebook or twitter',
+      },
+    ])
+  })
+
+  test('return array of top level functions for indexed array leaf', (assert) => {
+    const parsedRules = rulesParser({
+      'profiles': 'required',
+      'profiles.0.username': 'required',
+    })
+
+    const args = [
+      {
+        profiles: { required: 'profiles are required' },
+        'profiles.0.username': { required: 'primary profile must have username' },
+      },
+    ]
+
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'profiles',
+        dotPath: [],
+        type: 'array',
+        message: 'profiles are required',
+      },
+      [
+        {
+          child: {
+            rule: { name: 'required', args: [] },
+            field: 'username',
+            dotPath: [],
+            type: 'literal',
+            message: 'primary profile must have username',
+          },
+          dotPath: ['profiles'],
+          index: '0',
+        },
+      ],
+    ])
+  })
+
+  test('return array of top level functions for wildcard array leaf', (assert) => {
+    const parsedRules = rulesParser({
+      'profiles': 'required',
+      'profiles.*.username': 'required',
+    })
+
+    const args = [
+      {
+        profiles: { required: 'profiles are required' },
+        'profiles.*.username': { required: 'all profiles must have username' },
+      },
+    ]
+
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'profiles',
+        dotPath: [],
+        type: 'array',
+        message: 'profiles are required',
+      },
+      [
+        {
+          child: {
+            rule: { name: 'required', args: [] },
+            field: 'username',
+            dotPath: [],
+            type: 'literal',
+            message: 'all profiles must have username',
+          },
+          dotPath: ['profiles'],
+          index: '*',
+        },
+      ],
+    ])
+  })
+
+  test('return array of top level functions for nested object inside array', (assert) => {
+    const parsedRules = rulesParser({
+      'profiles': 'required',
+      'profiles.*.social': 'required',
+      'profiles.*.social.username': 'required',
+    })
+
+    const args = [
+      {
+        profiles: { required: 'profiles are required' },
+        'profiles.*.social': { required: 'all profiles must have social account' },
+        'profiles.*.social.username': { required: 'username for social profile is required' },
+      },
+    ]
+
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'profiles',
+        dotPath: [],
+        type: 'array',
+        message: 'profiles are required',
+      },
+      [
+        {
+          child: {
+            rule: { name: 'required', args: [] },
+            field: 'social',
+            dotPath: [],
+            type: 'object',
+            message: 'all profiles must have social account',
+          },
+          dotPath: ['profiles'],
+          index: '*',
+        },
+        {
+          child: {
+            rule: { name: 'required', args: [] },
+            field: 'username',
+            dotPath: ['social'],
+            type: 'literal',
+            message: 'username for social profile is required',
+          },
+          dotPath: ['profiles'],
+          index: '*',
+        },
+      ],
+    ])
+  })
+
+  test('return array of top level functions for deeply nested array', (assert) => {
+    const parsedRules = rulesParser({
+      'profiles': 'required',
+      'profiles.*.social': 'required',
+      'profiles.*.social.*.username': 'required',
+    })
+
+    const args = [
+      {
+        profiles: { required: 'profiles are required' },
+        'profiles.*.social': { required: 'all profiles must have social account' },
+        'profiles.*.social.*.username': { required: 'username for social profile is required' },
+      },
+    ]
+
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'profiles',
+        dotPath: [],
+        type: 'array',
+        message: 'profiles are required',
+      },
+      [
+        {
+          child: {
+            rule: { name: 'required', args: [] },
+            field: 'social',
+            dotPath: [],
+            type: 'array',
+            message: 'all profiles must have social account',
+          },
+          dotPath: ['profiles'],
+          index: '*',
+        },
+        {
+          child: [
+            {
+              child: {
+                rule: { name: 'required', args: [] },
+                field: 'username',
+                dotPath: [],
+                type: 'literal',
+                message: 'username for social profile is required',
+              },
+              dotPath: ['social'],
+              index: '*',
+            },
+          ],
+          dotPath: ['profiles'],
+          index: '*',
+        },
+      ],
+    ])
+  })
+
+  test('return array of top level functions first literal inside array', (assert) => {
+    const parsedRules = rulesParser({
+      'codes': 'required',
+      'codes.*': 'number',
+    })
+
+    const args = [
+      {
+        codes: { required: 'codes are required' },
+        'codes.*': { number: 'each item inside code must be number' },
+      },
+    ]
+
+    const output = astWalker(parsedRules, args, consumer, arrayWrapper).map((node) => node)
+    assert.deepEqual(output, [
+      {
+        rule: { name: 'required', args: [] },
+        field: 'codes',
+        dotPath: [],
+        type: 'array',
+        message: 'codes are required',
+      },
+      [
+        {
+          child: {
+            rule: { name: 'number', args: [] },
+            field: '*',
+            dotPath: [],
+            type: 'literal',
+            message: 'each item inside code must be number',
+          },
+          dotPath: ['codes'],
+          index: '*',
+        },
+      ],
+    ])
+  })
+})
