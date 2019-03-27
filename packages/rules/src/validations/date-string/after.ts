e68c972fceefa0c18b40f6dd8d388c02c9f596e4
Harminder virk <virk.officials@gmail.com>
2019-03-27 16:43:58 +0530

    refactor(rules): finalize after rules

---
+++ packages/rules/src/validations/date-string/after.ts
@@ -0,0 +1,54 @@
+/*
+* indicative
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import { skippable, args as argsValidator } from 'indicative-utils'
+import { Validation } from 'indicative-compiler'
+
+import { after } from '../../raw/after'
+import { ArgComparisonDate, RulesConfig } from '../../Contracts'
+
+const MISSING_VALUE = 'after:make sure to define the after date'
+const INVALID_TYPE = 'after:after date must be defined as string or date object'
+
+/**
+ * Ensures the value of the field is after the expected date.
+ * This method uses [isAfter](https://date-fns.org/docs/isAfter) function of date-fns.
+ *
+ * Validation fails if value is not a string or Date object.
+ *
+ * ```js
+ * const rules = {
+ *   confCall: `after:${new Date()}`
+ * }
+ *
+ * // or
+ * const rules = {
+ *   confCall: [
+ *     rules.after(new Date())
+ *   ]
+ * }
+ * ```
+ */
+const validation: Validation = {
+  async: false,
+
+  compile (args: any[]): any[] {
+    argsValidator.ensureLength(args, MISSING_VALUE, 1)
+
+    const afterDate = argsValidator.changeType(args[0], 'date', INVALID_TYPE)
+    return [afterDate]
+  },
+
+  validate: (data, field, [comparisonDate]: ArgComparisonDate, _type, _root, config: RulesConfig) => {
+    const fieldValue = data[field]
+    return skippable(fieldValue, field, config) || after(fieldValue, comparisonDate)
+  },
+}
+
+export { validation as default }
