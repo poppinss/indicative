009d429307e65c1963b3f593c7160baf8bdd5e44
Harminder virk <virk.officials@gmail.com>
2019-03-27 18:20:42 +0530

    refactor(rules): finalize requiredWhen rule

---
+++ packages/rules/src/validations/existence/requiredWhen.ts
@@ -0,0 +1,53 @@
+/*
+* indicative
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import { args as argsValidator } from 'indicative-utils'
+import { Validation } from 'indicative-compiler'
+import { empty } from '../../raw/empty'
+
+const MISSING_VALUES = 'requiredWhen:make sure to define target field and it\'s expected value'
+
+/**
+ * The field is checked for required validation, when target field value is same
+ * as the target value.
+ *
+ * ----
+ * const rules = {
+ *   state: 'required_when:country,US'
+ * }
+ *
+ * // or
+ * const rules = {
+ *   state: [
+ *     rule('required_when', ['country', 'US'])
+ *   ]
+ * }
+ * ----
+ */
+const validation: Validation = {
+  async: false,
+
+  compile (args): any[] {
+    argsValidator.ensureLength(args, MISSING_VALUES, 2)
+    return [String(args[0]), args[1]]
+  },
+
+  validate: (data, field, [targetField, expectedValue]: [string, any]) => {
+    const otherValue = data[targetField]
+
+    // tslint:disable-next-line:triple-equals
+    if (empty(otherValue) || expectedValue !== otherValue) {
+      return true
+    }
+
+    return !empty(data[field])
+  },
+}
+
+export { validation as default }
