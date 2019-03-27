99a0034fcf281f007146389860dbf541c2fe72dd
Harminder virk <virk.officials@gmail.com>
2019-03-27 19:18:07 +0530

    refactor(rules): finalize number rule

---
+++ packages/rules/src/validations/number/number.ts
@@ -0,0 +1,71 @@
+/*
+* indicative
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import { casts, skippable } from 'indicative-utils'
+import { Validation } from 'indicative-compiler'
+import { RulesConfig } from '../../Contracts'
+
+/**
+ * Makes sure that the value of field under validation is a valid
+ * whole number.
+ *
+ * Validation fails when number has a negative value. You must use `integer`
+ * or `float` rules for that.
+ *
+ * [casts]
+ *  [from label="string"]
+ *  [to label="number"]
+ * [/casts]
+ *
+ * ----
+ * const rules = {
+ *   game_points: 'number'
+ * }
+ *
+ * // or
+ * const rules = {
+ *   game_points: [
+ *     rules.number()
+ *   ]
+ * }
+ * ----
+ */
+const validation: Validation = {
+  async: false,
+
+  validate: (data, field, _args, _type, _root, config: RulesConfig) => {
+    let fieldValue = data[field]
+    if (skippable(fieldValue, field, config)) {
+      return true
+    }
+
+    /**
+     * Ensure value is castable to number
+     */
+    const castedValue = casts.toNumber(fieldValue)
+    if (castedValue === null) {
+      return false
+    }
+
+    /**
+     * Whole numbers must be greater than zero
+     */
+    if (castedValue < 0) {
+      return false
+    }
+
+    /**
+     * Mutate field value
+     */
+    data[field] = castedValue
+    return true
+  },
+}
+
+export { validation as default }
