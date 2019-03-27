11b005691b1388c5fe5a50b2a785810d209a0fef
Harminder virk <virk.officials@gmail.com>
2019-03-28 00:03:11 +0530

    refactor(rules): finalize ip rules

---
+++ packages/rules/src/validations/string/ipv6.ts
@@ -0,0 +1,42 @@
+/*
+* indicative
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import { skippable } from 'indicative-utils'
+import { Validation } from 'indicative-compiler'
+
+import { ipv6 } from '../../raw/ipv6'
+import { RulesConfig } from '../../Contracts'
+
+/**
+ * Ensures the value is a valid ip address as per `ipv6` spec only.
+ *
+ * [source, js]
+ * ----
+ * const rules = {
+ *   ip_address: 'ipv6'
+ * }
+ *
+ * // or
+ * const rules = {
+ *   ip_address: [
+ *     rule('ipv6')
+ *   ]
+ * }
+ * ----
+ */
+const validation: Validation = {
+  async: false,
+
+  validate: (data, field, _args, _type, _root, config: RulesConfig) => {
+    const fieldValue = data[field]
+    return skippable(fieldValue, field, config) || ipv6(fieldValue)
+  },
+}
+
+export { validation as default }
