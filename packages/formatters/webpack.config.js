433000de476ca3c596b931ed468af6115a73e100
Harminder virk <virk.officials@gmail.com>
2019-03-28 13:46:14 +0530

    feat(formatters): add vanilla formatter

---
+++ packages/formatters/webpack.config.js
@@ -0,0 +1,8 @@
+const { join } = require('path')
+
+module.exports = require('../../_webpack.config')({
+  output: {
+    path: join(__dirname, './build'),
+    library: 'indicative.parser',
+  }
+})
