758e5da1cb351068fd768d362669dd142ac62ec7
Harminder virk <virk.officials@gmail.com>
2019-03-28 13:08:20 +0530

    refactor(utils): move cast functions to a single method `cast`
    BREAKING CHANGE: Remove individual cast functions in favor of
    `cast` method

--- packages/utils/test/casts.spec.ts
+++ packages/utils/test/casts.spec.ts
@@ -8,153 +8,149 @@
 */
 
 import * as test from 'japa'
-import { toBoolean } from '../src/toBoolean'
-import { toDate } from '../src/toDate'
-import { toInt } from '../src/toInt'
-import { toNumber } from '../src/toNumber'
-import { toString } from '../src/toString'
+import { cast } from '../src/cast'
 
 test.group('toBoolean', () => {
   test('return boolean values as it is', (assert) => {
-    assert.isFalse(toBoolean(false))
-    assert.isTrue(toBoolean(true))
+    assert.isFalse(cast(false, 'boolean'))
+    assert.isTrue(cast(true, 'boolean'))
   })
 
   test('return string counterparts as boolean', (assert) => {
-    assert.isFalse(toBoolean('false'))
-    assert.isTrue(toBoolean('true'))
+    assert.isFalse(cast('false', 'boolean'))
+    assert.isTrue(cast('true', 'boolean'))
   })
 
   test('return number counterparts as boolean', (assert) => {
-    assert.isFalse(toBoolean(0))
-    assert.isTrue(toBoolean(1))
+    assert.isFalse(cast(0, 'boolean'))
+    assert.isTrue(cast(1, 'boolean'))
   })
 
   test('return string number counterparts as boolean', (assert) => {
-    assert.isFalse(toBoolean('0'))
-    assert.isTrue(toBoolean('1'))
+    assert.isFalse(cast('0', 'boolean'))
+    assert.isTrue(cast('1', 'boolean'))
   })
 
   test('return null for non-castable types', (assert) => {
-    assert.isNull(toBoolean({}))
-    assert.isNull(toBoolean([]))
-    assert.isNull(toBoolean(null))
-    assert.isNull(toBoolean(undefined))
-    assert.isNull(toBoolean(10))
-    assert.isNull(toBoolean('hello'))
+    assert.isNull(cast({}, 'boolean'))
+    assert.isNull(cast([], 'boolean'))
+    assert.isNull(cast(null, 'boolean'))
+    assert.isNull(cast(undefined, 'boolean'))
+    assert.isNull(cast(10, 'boolean'))
+    assert.isNull(cast('hello', 'boolean'))
   })
 })
 
 test.group('toDate', () => {
   test('return dates as it is', (assert) => {
     const today = new Date()
-    assert.equal(toDate(today), today)
+    assert.equal(cast(today, 'date'), today)
   })
 
   test('return string counterparts as dates', (assert) => {
     const today = new Date()
-    assert.equal(toDate(today.toString())!.toString(), today.toString())
+    assert.equal(cast(today.toString(), 'date')!.toString(), today.toString())
   })
 
   test('return invalid string counterparts as null', (assert) => {
-    assert.isNull(toDate('hello'))
+    assert.isNull(cast('hello', 'date'))
   })
 
   test('return null for non-castable types', (assert) => {
-    assert.isNull(toDate('hello'))
-    assert.isNull(toDate({}))
-    assert.isNull(toDate([]))
-    assert.isNull(toDate(null))
-    assert.isNull(toDate(undefined))
-    assert.isNull(toDate(10))
-    assert.isNull(toDate(true))
+    assert.isNull(cast('hello', 'date'))
+    assert.isNull(cast({}, 'date'))
+    assert.isNull(cast([], 'date'))
+    assert.isNull(cast(null, 'date'))
+    assert.isNull(cast(undefined, 'date'))
+    assert.isNull(cast(10, 'date'))
+    assert.isNull(cast(true, 'date'))
   })
 })
 
 test.group('toInt', () => {
   test('return integers as it is', (assert) => {
-    assert.equal(toInt(10), 10)
+    assert.equal(cast(10, 'integer'), 10)
   })
 
   test('drop decimal values from int', (assert) => {
-    assert.equal(toInt(10.12), 10)
+    assert.equal(cast(10.12, 'integer'), 10)
   })
 
   test('return string counterparts as integers', (assert) => {
-    assert.equal(toInt('10'), 10)
+    assert.equal(cast('10', 'integer'), 10)
   })
 
   test('return string counterparts with decimal as integers', (assert) => {
-    assert.equal(toInt('10.60'), 10)
+    assert.equal(cast('10.60', 'integer'), 10)
   })
 
   test('return null for non-castable types', (assert) => {
-    assert.isNull(toInt('hello'))
-    assert.isNull(toInt({}))
-    assert.isNull(toInt([]))
-    assert.isNull(toInt(null))
-    assert.isNull(toInt(undefined))
-    assert.isNull(toInt(new Date()))
-    assert.isNull(toInt(true))
+    assert.isNull(cast('hello', 'integer'))
+    assert.isNull(cast({}, 'integer'))
+    assert.isNull(cast([], 'integer'))
+    assert.isNull(cast(null, 'integer'))
+    assert.isNull(cast(undefined, 'integer'))
+    assert.isNull(cast(new Date(), 'integer'))
+    assert.isNull(cast(true, 'integer'))
   })
 })
 
 test.group('toNumber', () => {
   test('return number as it is', (assert) => {
-    assert.equal(toNumber(10), 10)
+    assert.equal(cast(10, 'number'), 10)
   })
 
   test('keep decimal on numbers', (assert) => {
-    assert.equal(toNumber(10.12), 10.12)
+    assert.equal(cast(10.12, 'number'), 10.12)
   })
 
   test('return string counterparts as numbers', (assert) => {
-    assert.equal(toNumber('10'), 10)
+    assert.equal(cast('10', 'number'), 10)
   })
 
   test('keep decimal on string counterparts', (assert) => {
-    assert.equal(toNumber('10.60'), 10.60)
+    assert.equal(cast('10.60', 'number'), 10.60)
   })
 
   test('return null for non-castable types', (assert) => {
-    assert.isNull(toNumber('hello'))
-    assert.isNull(toNumber({}))
-    assert.isNull(toNumber([]))
-    assert.isNull(toNumber(null))
-    assert.isNull(toNumber(undefined))
-    assert.isNull(toNumber(new Date()))
-    assert.isNull(toNumber(true))
+    assert.isNull(cast('hello', 'number'))
+    assert.isNull(cast({}, 'number'))
+    assert.isNull(cast([], 'number'))
+    assert.isNull(cast(null, 'number'))
+    assert.isNull(cast(undefined, 'number'))
+    assert.isNull(cast(new Date(), 'number'))
+    assert.isNull(cast(true, 'number'))
   })
 })
 
 test.group('toString', () => {
   test('return strings as it is', (assert) => {
-    assert.equal(toString('hello'), 'hello')
+    assert.equal(cast('hello', 'string'), 'hello')
   })
 
   test('return arrays as string', (assert) => {
-    assert.equal(toString([1,2]), '1,2')
+    assert.equal(cast([1,2], 'string'), '1,2')
   })
 
   test('return booleans as string', (assert) => {
-    assert.equal(toString(true), 'true')
+    assert.equal(cast(true, 'string'), 'true')
   })
 
   test('return booleans as string', (assert) => {
-    assert.equal(toString(false), 'false')
+    assert.equal(cast(false, 'string'), 'false')
   })
 
   test('return numbers as string', (assert) => {
-    assert.equal(toString(1), '1')
+    assert.equal(cast(1, 'string'), '1')
   })
 
   test('return numbers with decimals string', (assert) => {
-    assert.equal(toString(10.1), '10.1')
+    assert.equal(cast(10.1, 'string'), '10.1')
   })
 
   test('return null for non-castable types', (assert) => {
-    assert.isNull(toString(undefined))
-    assert.isNull(toString({}))
-    assert.isNull(toString(null))
+    assert.isNull(cast(undefined, 'string'))
+    assert.isNull(cast({}, 'string'))
+    assert.isNull(cast(null, 'string'))
   })
 })
