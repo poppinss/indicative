1343d6b0b8dbedda586ca75272b5700abd004158
Harminder virk <virk.officials@gmail.com>
2019-03-29 14:22:09 +0530

    test(formatters): add vanilla formatter tests

--- packages/formatters/src/VanillaFormatter/index.ts
+++ packages/formatters/src/VanillaFormatter/index.ts
@@ -9,15 +9,25 @@
 
 import { FormatterContract } from 'indicative-compiler'
 
+/**
+ * Shape of error node
+ */
 export type VanillaErrorNode = {
   error: string,
   validation: string,
   field: string,
 }
 
+/**
+ * Vanilla formatter is a plain formatter to collect validation
+ * errors
+ */
 export class VanillaFormatter implements FormatterContract {
   public errors: VanillaErrorNode[] = []
 
+  /**
+   * Adds error to the list of existing errors
+   */
   public addError (error: Error | string, field: string, rule: string) {
     let message = ''
 
@@ -31,6 +41,10 @@ export class VanillaFormatter implements FormatterContract {
     this.errors.push({ error: message, validation: rule, field: field })
   }
 
+  /**
+   * Returns an array of errors or `null` when there are no
+   * errors
+   */
   public toJSON (): VanillaErrorNode[] | null {
     return this.errors.length ? this.errors : null
   }
