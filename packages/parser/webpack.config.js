084ded9f010fe86406f42d3635abf1bcfcae9552
Harminder virk <virk.officials@gmail.com>
2019-06-11 13:22:23 +0530

    refactor: get rid of monorepo

--- packages/parser/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const { join } = require('path')
-
-module.exports = require('../../_webpack.config')({
-  output: {
-    path: join(__dirname, './build'),
-    library: 'indicative.parser',
-  }
-})
